e1c790e22e126ddd82b59443555206d55ea15725
Sandip Kundu <kundusandip004@gmail.com>
2025-08-24 21:11:42 +0530

    Create Histogram of Tweets.sql

---
+++ Easy/Histogram of Tweets.sql
@@ -0,0 +1,14 @@
+SELECT
+  COUNT(user_id) AS tweet_bucket,
+  users_num
+FROM 
+    (
+      SELECT 
+        user_id,
+        COUNT(user_id) AS users_num
+      FROM tweets
+      WHERE YEAR(tweet_date) = 2022
+      GROUP BY user_id
+    ) AS Count_Users
+GROUP BY users_num
+ORDER BY tweet_bucket
