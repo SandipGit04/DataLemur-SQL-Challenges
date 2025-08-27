fc23d9c16354bdffa48b3f66a2c246eb1e5f7106
Sandip Kundu <kundusandip004@gmail.com>
2025-08-27 12:29:26 +0530

    update

--- Easy/Page With No Likes.sql
+++ Easy/Page With No Likes.sql
@@ -1,6 +1,7 @@
--- Company: Facebook
+-- Problem Statement: Page With No Likes | Company: Facebook
 SELECT p.page_id
 FROM pages AS p
+  
 LEFT JOIN page_likes AS pl
 ON p.page_id = pl.page_id
 GROUP BY p.page_id
