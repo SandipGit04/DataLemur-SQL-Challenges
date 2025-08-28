af6e1f5e90195ba85391ee43985f4ea63b5d0fb8
Sandip Kundu <kundusandip004@gmail.com>
2025-08-28 12:33:06 +0530

    new file

---
+++ Easy/Teams Power Users.sql
@@ -0,0 +1,9 @@
+-- Problem Statement: Teams Power Users | Company: Microsoft
+SELECT 
+  sender_id,
+  COUNT(receiver_id) AS message_count
+FROM messages
+WHERE DATE_PART('year', sent_date) = 2022 AND DATE_PART('month', sent_date) = 8
+GROUP BY sender_id
+ORDER BY message_count DESC
+LIMIT 2;
