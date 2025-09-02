536de1368416285dc72d84fa0d70d2db525646a2
Sandip Kundu <kundusandip004@gmail.com>
2025-09-02 13:37:31 +0530

    upload

--- Easy/Final Account Balance.sql
+++ Easy/Final Account Balance.sql
@@ -2,6 +2,6 @@
 SELECT 
   account_id,
   SUM(CASE WHEN transaction_type = 'Deposit' THEN amount ELSE 0 END) - 
-  SUM(CASE WHEN transaction_type = 'Withdrawal' THEN amount ELSE 0 END)
+  SUM(CASE WHEN transaction_type = 'Withdrawal' THEN amount ELSE 0 END) 
 FROM transactions
 GROUP BY account_id;
