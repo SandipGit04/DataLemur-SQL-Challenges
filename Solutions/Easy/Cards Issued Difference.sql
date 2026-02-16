63f706ccb0cb26de90b3ee30949b9c6b217f1fe7
Sandip Kundu <kundusandip004@gmail.com>
2026-02-16 14:40:29 +0530

    solved
    Add SQL query to calculate the difference in issued amounts for each card.

---
+++ Solutions/Easy/Cards Issued Difference.sql
@@ -0,0 +1,7 @@
+-- Problem Statement: Cards Issued Difference | Company: JPMorgan
+SELECT 
+  card_name,
+  MAX(issued_amount) - MIN(issued_amount) AS difference
+FROM monthly_cards_issued
+GROUP BY card_name
+ORDER BY difference DESC;
