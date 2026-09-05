94f00666bb2f15b16dab11e20b85af436b59ce9b
Sandip Kundu <kundusandip004@gmail.com>
2026-09-05 13:44:37 +0530

    Delete Medium/Pharmacy Analytics (Part 1).sql

--- Medium/Pharmacy Analytics (Part 1).sql
+++
@@ -1,7 +0,0 @@
--- Problem Statement: Pharmacy Analytics (Part 1) | Company: CVS Health
-SELECT 
-  drug,
-  (total_sales - cogs) AS total_profit
-FROM pharmacy_sales
-ORDER BY total_profit DESC
-LIMIT 3;
